4f155024c60bf5477de59c0708bdfe0a5b537987
Gading <sutan.gnst@gmail.com>
2020-08-20 15:20:59 +0700

    update

--- server.ts
+++ server.ts
@@ -1,12 +1,10 @@
 import Express from 'express'
-import Cors from 'cors'
 import Api from './src/api'
 
 const server = Express()
 const port = process.env.PORT || 9600
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-server.use(Cors())
 server.use(Api)
 
 server.get('*', (_, res) => {
